112091fbcdee9acc34589d853fad1d4ef44da389
SumaiaSorna <sorna.sumaia@gmail.com>
2022-03-08 21:28:35 +0000

    files and folders and wip in the pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import { BrowserRouter } from "react-router-dom";
-import { NavBar } from "./components/NavBar";
-import { AppRouter } from "./components/Routes";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+import { AppRouter } from "./components/AppRouter";
+import { AppProvider } from "./contexts/AppProvider";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+  credentials: "same-origin",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <AppRouter />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <AppProvider>
+        <BrowserRouter>
+          {/* <NavBar /> */}
+          <AppRouter />
+        </BrowserRouter>
+      </AppProvider>
+    </ApolloProvider>
   );
 };
--- src/components/Routes.js
+++
@@ -1,3 +0,0 @@
-export const AppRouter = () => {
-  return <h1>AppRouter</h1>;
-};
