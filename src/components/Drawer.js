6745aa68828c795a6705d350572a81de088ea9eb
liana.valeria15 <liana.valeria15@gmail.com>
2022-03-12 11:42:29 +0000

    fixed drawer anchor top

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -6,53 +6,91 @@ import ListItemText from "@mui/material/ListItemText";
 import { Link } from "react-router-dom";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { makeStyles } from "@mui/styles";
+
+//css styling
+const useStyles = makeStyles(() => ({
+  drawer: {
+    backgroundColor: "blue",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+  },
+  icon: {
+    color: "white",
+  },
+}));
 
 export const DrawerComponent = () => {
+  const classes = useStyles();
+
   //hook to display drawer component
   const [openDrawer, setOpenDrawer] = useState(false);
 
   return (
     <>
-      <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
-        <List>
+      <Drawer
+        anchor="top"
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+      >
+        <List className={classes.drawer}>
           <ListItem onClick={() => setOpenDrawer(false)}>
             <ListItemText>
-              <Link to="/">Home</Link>
+              <Link to="/" className={classes.link}>
+                Home
+              </Link>
             </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)}>
             <ListItemText>
-              <Link to="/about">About Us</Link>
+              <Link to="/about" className={classes.link}>
+                About Us
+              </Link>
             </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)}>
             <ListItemText>
-              <Link to="/sign-up">Sign Up</Link>
+              <Link to="/sign-up" className={classes.link}>
+                Sign Up
+              </Link>
             </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)}>
             <ListItemText>
-              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/dashboard" className={classes.link}>
+                Dashboard
+              </Link>
             </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)}>
             <ListItemText>
-              <Link to="/job-board">Job Board</Link>
+              <Link to="/job-board" className={classes.link}>
+                Job Board
+              </Link>
             </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)}>
             <ListItemText>
-              <Link to="/buy-sell">Buy/Sell</Link>
+              <Link to="/buy-sell" className={classes.link}>
+                Buy/Sell
+              </Link>
             </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)}>
             <ListItemText>
-              <Link to="/forum-board">Forum Board</Link>
+              <Link to="/forum-board" className={classes.link}>
+                Forum Board
+              </Link>
             </ListItemText>
           </ListItem>
           <ListItem onClick={() => setOpenDrawer(false)}>
             <ListItemText>
-              <Link to="/edit-profile">Edit Profile</Link>
+              <Link to="/edit-profile" className={classes.link}>
+                Edit Profile
+              </Link>
             </ListItemText>
           </ListItem>
         </List>
