58afab7a3c8f7ed792222e0bc4a749a5e9b997f5
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-03-13 22:51:57 +0000

    notes for ViewCreatedJobs page

--- src/pages/ViewCreatedJobs.js
+++ src/pages/ViewCreatedJobs.js
@@ -1,3 +1,9 @@
 export const ViewCreatedJobs = () => {
+  // query jobs by staff id
+  // render JobCards with delete button & edit button
+  // onDelete delete the job and re-render list
+  // onEdit button direct to edit-job path/page
+  // error handling
+
   return <h1>Your Listed Job Openings</h1>;
 };
