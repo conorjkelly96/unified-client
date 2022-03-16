aa9c0a64ae36c09118fbeec1764afa811b782334
Conor Kelly <conorjkelly96@gmail.com>
2022-03-16 21:29:31 +0000

    removing console log and getting item data

--- src/pages/Marketplace.js
+++ src/pages/Marketplace.js
@@ -1,8 +1,36 @@
+import { useLazyQuery } from "@apollo/client";
 import { Divider } from "@mui/material";
 import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
 import { ItemCard } from "../components/ItemCard";
+import { VIEW_ALL_ITEMS } from "../queries";
 
 export const Marketplace = () => {
+  const [executeViewAllItems, { loading: allItemsLoading }] =
+    useLazyQuery(VIEW_ALL_ITEMS);
+
+  const [itemData, setItemData] = useState([]);
+
+  useEffect(() => {
+    const getItemData = async () => {
+      try {
+        const { data: itemData, error: itemDataError } =
+          await executeViewAllItems();
+
+        if (itemDataError) {
+          throw new Error("Something went wrong.");
+        }
+
+        console.log("itemData:", itemData);
+
+        setItemData(itemData.viewAllItem);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getItemData();
+  }, [itemData, executeViewAllItems]);
+
   return (
     <>
       <Divider sx={{ maxWidth: "90%", margin: "auto" }} />
