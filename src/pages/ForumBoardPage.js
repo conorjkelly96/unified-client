2feb06bdcd536311c59c3ad105129c1c9428367b
Kayle Rieger Patton <kayle.patton22@gmail.com>
2022-03-17 20:33:19 +0100

    create post button

--- src/pages/ForumBoardPage.js
+++ src/pages/ForumBoardPage.js
@@ -1,12 +1,19 @@
 import { useQuery } from "@apollo/client";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import { GET_FORUM_POSTS } from "../queries";
 import { Error } from "./Error";
 import { ForumPostCard } from "../components/ForumPostCard";
+import Stack from "@mui/material/Stack";
+import { useTheme } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 
 export const ForumBoardPage = () => {
   const { data, loading, error } = useQuery(GET_FORUM_POSTS);
+
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   if (error) {
     return <Error />;
@@ -28,6 +35,17 @@ export const ForumBoardPage = () => {
 
   return (
     <>
+      <Box sx={{ maxWidth: "750px", margin: "auto" }}>
+        <Stack
+          direction="row"
+          justifyContent={isMobile ? "center" : "end"}
+          sx={{ marginTop: "32px" }}
+        >
+          <Button variant="contained" component="a" href="/create-post">
+            Post Question
+          </Button>
+        </Stack>
+      </Box>
       <Typography
         variant="h4"
         gutterBottom
