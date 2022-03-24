7ddf611c3ad8e257c8fb505b72f6862e24d8c2f4
Conor Kelly <conorjkelly96@gmail.com>
2022-03-24 12:22:34 +0000

    staff navbar hotfix

--- src/pages/Marketplace.js
+++ src/pages/Marketplace.js
@@ -118,7 +118,7 @@ export const Marketplace = () => {
     <>
       <Container
         component={"main"}
-        maxWidth={"xs"}
+        maxWidth={1200}
         sx={{ ...mainContainer, boxShadow: "none" }}
       >
         <Spinner loading={itemLoading} />
